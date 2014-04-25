b0ac75ea0efd1c252e46bcab8b543bcf7a716048
xuxincheng66 <yogixxc@gmail.com>
2014-04-25 18:30:53 +0800

    增加首页接口

--- demo/js/backstage-index.js
+++ demo/js/backstage-index.js
@@ -3,7 +3,33 @@ $('.img-add').live('click', function() {
 });
 
 $('#add-course').click(function() {
-	$('.course-main').append('<div><label>重点学科：</label><input class="form-control" type="text" placeholder="标题" /><input class="form-control" type="text" placeholder="链接"/></div>')
+	$('.course-content').append('<div class="course-main clearfix"><label>重点学科：</label><input class="form-control" type="text" placeholder="标题" /><input class="form-control" type="text" placeholder="链接"/></div>')
+});
+
+$('#save-course').click(function() {
+	var index = $('.course-main').length;
+	for( var i = 0; i < index; i ++){		
+		var courseId = $('.course-main').eq(i).find('.course-id').val,
+			courseLink = $('.course-main').eq(i).find('.course-link').val;
+		$.ajax({
+		  	type: 'POST',
+		  	url: '/admin/index/insertHotType?level=1&typeId='+courseId+'&desc='+courseLink
+		});
+	}
+});
+
+$('#save-course').click(function() {
+	var index = $('.course-img li').length;
+	for( var i = 0; i < index; i ++){		
+		var textimgImgurl = $('.course-img li').eq(i).find('.textimg-imgurl').val,
+			textimgTitle = $('.course-img li').eq(i).find('.textimg-titlerl').val,
+			textimgText = $('.course-img li').eq(i).find('.textimg-text').val,
+			textimgLink = $('.course-img li').eq(i).find('.textimg-link').val;
+		$.ajax({
+		  	type: 'POST',
+		  	url: /admin/index/insertHotType?level=2&typeId=type_id&desc=desc&img=img(type_id:学科id,img:展示图片)
+		});
+	}
 });
 
 $('.course-img i').live('click',function(){
@@ -17,3 +43,19 @@ $('.course-img i').live('click',function(){
 		$('.will-delete').hide();
 	});
 })
+
+$('#webinfo-save').click(function() {
+	var webInfo = $('#webinfo-text').val();
+	$.ajax({
+	  	type: 'POST',
+	  	url: '/admin/index/update?id=1&text='+webInfo
+	});
+});
+
+$('#examinfo-save').click(function() {
+	var webInfo = $('#examinfo-text').val();
+	$.ajax({
+	  	type: 'POST',
+	  	url: '/admin/index/update?id=2&text='+webInfo
+	});
+});
