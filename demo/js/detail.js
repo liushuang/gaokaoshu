3f77852919c48eb843968557e492ad678a7f4ea3
xuxincheng66 <yogixxc@gmail.com>
2014-05-01 20:34:08 +0800

    debug

--- demo/js/detail.js
+++ demo/js/detail.js
@@ -10,19 +10,11 @@ $.ajax({
 		$('.single-panel-header h3').text(contentAll.blogtitle);
 		for( var i = 0; i < index; i ++ ){
 			$('.single-panel-body').append( '<h4 class="blog-title" id="blog-title' + i + '">' + contentAll.title[i] + '</h4>' +  contentAll.text[i]);
-			$('.nav').append( '<li><a data-local="' + i + '">' + contentAll.title[i] + '</a></li>');
+			$('.nav').append( '<li><a href="javascript:void(0)" data-local="' + i + '">' + contentAll.title[i] + '</a></li>');
 		};
 	}
 });
 
-// var data = '{"blogtitle":"123","title":["123","456"],"text":["123","456"]}';
-// var contentAll = eval('(' + data + ')'); 
-// var index = contentAll.text.length;
-// $('.single-panel-header h3').text(contentAll.blogtitle);
-// for( var i = 0; i < index; i ++ ){
-// 	$('.single-panel-body').append( '<h4 class="blog-title" id="blog-title' + i + '">' + contentAll.title[i] + '</h4>' +  contentAll.text[i]);
-// 	$('.nav').append( '<li><a data-local="' + i + '">' + contentAll.title[i] + '</a></li>');
-// };
 $('.nav a').live('click',function(){	
 	var domId = $(this).attr('data-local');
 	$("html,body").animate({scrollTop:$("#blog-title" + domId).offset().top - 80},300);
