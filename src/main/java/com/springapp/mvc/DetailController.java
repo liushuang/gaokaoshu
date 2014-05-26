355b9cca6edc5b5aee10d3ff6d61335526343a9d
liushuang <tlnrf@sina.com>
2014-05-26 16:49:56 +0800

    add vote

--- src/main/java/com/springapp/mvc/DetailController.java
+++ src/main/java/com/springapp/mvc/DetailController.java
@@ -5,10 +5,15 @@ import com.gaokaoshu.dao.TypeDAO;
 import com.gaokaoshu.dao.ViewCountDAO;
 import com.gaokaoshu.entity.BlogEntity;
 import com.gaokaoshu.entity.TypeEntity;
+import com.gaokaoshu.util.AdminUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.sql.Timestamp;
 
 /**
  * 详情页
@@ -40,5 +45,22 @@ public class DetailController {
         model.addAttribute("type", typeEntity);
         model.addAttribute("viewCount",viewCount);
         return "detail";
+    }
+
+    @RequestMapping("/blog/vote")
+    @ResponseBody
+    public String addBlog(HttpSession session, int typeId, int vote_type) {
+        BlogEntity blogEntity = BlogDAO.getBlogByTypeId(typeId);
+        if(blogEntity != null){
+            if(vote_type == 1){// 1代表赞成
+                blogEntity.setGoodCount(blogEntity.getGoodCount() + 1);
+            }else if(vote_type == 2){// 2代表中立
+                blogEntity.setMiddleCount(blogEntity.getMiddleCount() + 1);
+            }else if(vote_type == 3){// 3代表踩
+                blogEntity.setBadCount(blogEntity.getBadCount() + 1);
+            }
+            BlogDAO.updateBlogEntity(blogEntity);
+        }
+        return "success";
     }
 }
